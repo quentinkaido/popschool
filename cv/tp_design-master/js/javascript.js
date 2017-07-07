b9666c6bc41b995fdd352e1272ac943b203950a2
Quentin Leroux <quentin.leroux@hotmail.com>
2017-07-07 19:25:36 +0200

    ajout site cv

---
+++ cv/tp_design-master/js/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#menu_1").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+
+$(function () {
+    $('.tlt').textillate({ in: { effect: 'rollIn' } });
+})
