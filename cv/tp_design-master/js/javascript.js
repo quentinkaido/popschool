1bf7f548939faa413ba7afc1e684f59d233e7337
Quentin Leroux <quentin.leroux@hotmail.com>
2017-07-10 09:36:36 +0200

    modif

--- cv/tp_design-master/js/javascript.js
+++ cv/tp_design-master/js/javascript.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-  $("#menu_1").click(function(){
-    $("#panel").slideToggle("slow");
-  });
-});
+// $(document).ready(function(){
+//   $("#menu_1").click(function(){
+//     $("#panel").slideToggle("slow");
+//   });
+// });
+
+// $('#menu_1').click(function() {
+//    $('#panel').slideToggle('slow');
+//    return false;
+// });
