c3a37a40941537d840bdb2fab2cc2e933d3df9b2
Quentin Leroux <quentin.leroux@hotmail.com>
2017-07-18 17:15:28 +0200

    Ajout suite fibonnacia et tableau

--- Juillet/180717/exo2/assets/js/javascript.js
+++ Juillet/180717/exo2/assets/js/javascript.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var myArray = ["kevin", "jean-paul", "bernard", "bénédicte", "robert", "sébastiane"];
+var myArray = ["kevin", "jean-paul", "bernard", "bénédicte", "robert", "sébastiane", 45, 89, 2, 51];
 var myArray2 = [];
 
 //boucle for avec un index qui commence au début
@@ -14,10 +14,32 @@ for (var i = myArray.length -1; i >= 0; i--){
 }
 
 
-//myArray2
-
 for (var i = 0; i < 10; i++){
   myArray.push(myArray2);
 }
 
-console.log(myArray2);
+
+//afficher les nombre au dessus de 50
+for (var i = 0; i < myArray.length; i++){
+  if (myArray[i] > 50){
+    console.log(myArray[i]);
+  }
+}
+
+
+function fibonacci()
+{
+  var nb_1 = 0, nb_2 = 1;
+  for(var j = 0; j < 500; j++)
+
+  {
+    var result = nb_1 + nb_2;
+    console.log(result);
+    nb_1 = nb_2;
+    nb_2 = result;
+    if (result > 500000){
+      console.log(result[j]);
+    }
+  }
+}
+fibonacci();
